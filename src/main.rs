933f078e7fb4595a6fc7f4b6103f65377c4587fe
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-11-26 11:52:51 -0500

    Remove: line that prints the secret number, which ruins the game if left in place.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1,101);
 
-    // below line prints secret number for dev proccess
-    println!("The Secret Number is: {}", secret_number);
-
     loop {
         println!("Please input your guess.");
 

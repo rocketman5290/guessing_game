7241d542d5a7a9156c64bf5f8bae3cbc10cb34c5
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-11-25 21:24:18 -0500

    Add: match statement with type error example on cargo build

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use std::io;
+use std::{io, cmp::Ordering};
+// use std::cmp::Ordering;
 use rand::Rng;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1,101);
+
     // below line prints secret number for dev proccess
     println!("The Secret Number is: {}", secret_number);
+
     println!("Please input your guess.");
+
     let mut guess = String::new();
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("To small"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
